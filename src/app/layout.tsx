b282c759752ba05662a0a979d2666952748fff01
Nijat Hamid <nicatorium@gmail.com>
2024-08-14 17:12:24 +0400

    SEO optimizations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Header } from "@/components/shared/header";
 import { Footer } from "@/components/shared/footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -13,7 +14,56 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: "Vilicos - Community Agent",
   description: "Automated Community Support Agent for Web 3",
-  
+  keywords: ['Community Support', 'Automated Web 3 Agent', 'AI Agent', 'Web 3 Agent', 'AI Web 3 Agent',],
+  applicationName:"Vilicos",
+  authors:[{name:"Vilicos Inc.",url:"https://vilicos.com"}],
+  creator:"Nijat Hamid",
+  publisher:"Orkhan Aslanov",
+  referrer:"origin-when-cross-origin",
+  manifest:"https://vilicos.com/metadata/manifest.webmanifest",
+  category:'artificial intelligence',
+  twitter:{
+    card:'summary_large_image',
+    title:'Vilicos - Community Agent',
+    description:"Automated Community Support Agent for Web 3",
+    creator:'@vilicos',
+    images:{url:'https://vilicos.com/metadata/og-image.png',alt:"Vilicos"},
+    siteId:"1814744523056021504",
+    creatorId:'1814744523056021504',
+    site:"@vilicos",
+  },
+  openGraph:{
+    title:"Vilicos - Community Agent",
+    description:"Automated Community Support Agent for Web 3",
+    url:"https://vilicos.com",
+    siteName:"Vilicos",
+    type:'website',
+    locale:'en_US',
+    images:[{
+      url:"https://vilicos.com/metadata/og-image.png",
+      alt:"Vilicos",
+      width:1200,
+      height:769,
+      type:"image/png"
+    }]
+  },
+  icons:{
+    icon:[{
+      url:'/metadata/favicon-32x32.png',
+      sizes:'32x32'
+    },
+    {
+      url:'/metadata/favicon-16x16.png',
+      sizes:'16x16'
+    }
+  ],
+    
+   apple:{url:"/metadata/apple-touch-icon.png",sizes:'180x180'},
+   other:{
+      rel:'mask-icon',
+      url:'/safari-pinned-tab.svg'
+   }
+  }
 };
 
 export default function RootLayout({
@@ -28,6 +78,7 @@ export default function RootLayout({
         <main className="container min-h-[calc(100dvh_-_216px)]">{children}</main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId={`G-${process.env.MEASUREMENT_ID}`} />
     </html>
   );
 }
